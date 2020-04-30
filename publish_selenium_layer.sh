1af20505fdd45243d4e2b6d5238f5954f0695232
umihico <umihico@users.noreply.github.com>
2020-05-01 07:21:10 +0900

    add layer selenium binary included

---
+++ publish_selenium_layer.sh
@@ -0,0 +1,10 @@
+mkdir -p python/bin/
+curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-37/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+unzip headless-chromium.zip -d python/bin/
+curl -SL https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip > chromedriver.zip
+unzip chromedriver.zip -d python/bin/
+rm -rf chromedriver.zip headless-chromium.zip
+docker run --rm -v $(pwd):/var/task -w /var/task lambci/lambda:build-python3.7 pip install selenium -t ./python
+zip -r layer.zip python
+aws lambda publish-layer-version --layer-name selenium_with_bin --zip-file fileb://layer.zip --compatible-runtimes python3.7 --region ap-northeast-1
+rm -rf layer.zip python
